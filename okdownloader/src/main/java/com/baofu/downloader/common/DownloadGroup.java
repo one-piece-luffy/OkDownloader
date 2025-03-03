d22195f5157f072a448afb7974adf7e458541b5d
one piece <646796992@qq.com>
2025-03-03 16:11:32 +0800

    1

---
+++ okdownloader/src/main/java/com/baofu/downloader/common/DownloadGroup.java
@@ -0,0 +1,8 @@
+package com.baofu.downloader.common;
+
+public interface DownloadGroup {
+    //ins
+    public static final String INSTAGRAM="instagram";
+    //web
+    public static final String WEB="web";
+}
