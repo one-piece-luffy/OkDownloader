a3366d3701d34bfd10c6f4ca6ac5e36c5bdbfb92
one piece <646796992@qq.com>
2025-03-04 10:06:53 +0800

    1

--- okdownloader/src/main/java/com/baofu/downloader/database/VideoDownloadSQLiteHelper.java
+++ okdownloader/src/main/java/com/baofu/downloader/database/VideoDownloadSQLiteHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class VideoDownloadSQLiteHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "video_download_info.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public static final String TABLE_VIDEO_DOWNLOAD_INFO = "video_download_info";
 
